3e093f1507667065a70f4aa99e13f136877d8175
stevenconner <steven.c.conner@gmail.com>
2016-12-08 13:28:36 -0800

    all buttons currently working, ready for new features

--- app/src/main/java/sigildesigns/materiallifecounter/NewGameActivity.java
+++ app/src/main/java/sigildesigns/materiallifecounter/NewGameActivity.java
@@ -1,13 +1,23 @@
 package sigildesigns.materiallifecounter;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class NewGameActivity extends AppCompatActivity {
+
+    // Spinner to change the amount of players
+    private Spinner mPlayersSpinner;
+
+    // Number of players
+    private int mPlayers = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +26,54 @@ public class NewGameActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.new_game_fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.start_game_fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(NewGameActivity.this, TwoPlayerActivity.class);
+                startActivity(intent);
             }
         });
+
+        // Find all relevant views that we will need to read user input from
+        mPlayersSpinner = (Spinner) findViewById(R.id.spinner_players);
+
+        // Set up the spinner
+        setupSpinner();
     }
 
+    /**
+     * Setup the dropdown spinner that allows the user to select the amount of players.
+     */
+    private void setupSpinner() {
+        // Create adapter for spinner. The list options are from the String array it will use
+        // the default layout.
+        ArrayAdapter playersSpinnerAdapter = ArrayAdapter.createFromResource(this, R.array
+                .array_player_options, android.R.layout.simple_spinner_item);
+
+        // Specify dropdown layout style - siple list view with 1 item per line.
+        playersSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+
+        // Apply the adapter to the spinner
+        mPlayersSpinner.setAdapter(playersSpinnerAdapter);
+
+        // Set the integer mPlayers to the amount of players, currently defaults to 2
+        mPlayersSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selection = (String) parent.getItemAtPosition(position);
+                if (!TextUtils.isEmpty(selection)) {
+                    mPlayers = 2;
+                } else if (selection.equals(getString(R.string.players_two))) {
+                    mPlayers = 2;
+                }
+            }
+            // Because AdapterView is an abstract class, onNothingSelected must be defined.
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                mPlayers = 2;
+            }
+        });
+
+    }
 }
--- app/src/main/java/sigildesigns/materiallifecounter/TwoPlayerActivity.java
+++ app/src/main/java/sigildesigns/materiallifecounter/TwoPlayerActivity.java
@@ -1,13 +1,224 @@
 package sigildesigns.materiallifecounter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class TwoPlayerActivity extends AppCompatActivity {
+
+    private Button player1MinusOneHealth;
+    private Button player1MinusFiveHealth;
+    private TextView player1HealthView;
+    private Button player1PlusOneHealth;
+    private Button player1PlusFiveHealth;
+    private Button player1MinusEnergy;
+    private TextView player1EnergyView;
+    private Button player1PlusEnergy;
+    private Button player1MinusPoison;
+    private TextView player1PoisonView;
+    private Button player1PlusPoison;
+    private Button player2MinusOneHealth;
+    private Button player2MinusFiveHealth;
+    private TextView player2HealthView;
+    private Button player2PlusOneHealth;
+    private Button player2PlusFiveHealth;
+    private Button player2MinusEnergy;
+    private TextView player2EnergyView;
+    private Button player2PlusEnergy;
+    private Button player2MinusPoison;
+    private TextView player2PoisonView;
+    private Button player2PlusPoison;
+
+    // Variables for tracking player health, energy, and poison
+    private int healthPlayer1 = 20;
+    private int healthPlayer2 = 20;
+    private int poisonPlayer1 = 0;
+    private int poisonPlayer2 = 0;
+    private int energyPlayer1 = 0;
+    private int energyPlayer2 = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_two_player);
+
+        // Store the views and buttons in variables.
+        Button player1MinusOneHealth = (Button) findViewById(R.id.two_player_p1_minus_onehealth);
+        Button player1MinusFiveHealth = (Button) findViewById(R.id.two_player_p1_minus_fivehealth);
+
+        Button player1PlusOneHealth = (Button) findViewById(R.id.two_player_p1_plus_onehealth);
+        Button player1PlusFiveHealth = (Button) findViewById(R.id.two_player_p1_plus_fivehealth);
+        Button player1MinusEnergy = (Button) findViewById(R.id.two_player_p1_minus_energy);
+        TextView player1EnergyView = (TextView) findViewById(R.id.two_player_p1_energy);
+        Button player1PlusEnergy = (Button) findViewById(R.id.two_player_p1_plus_energy);
+        Button player1MinusPoison = (Button) findViewById(R.id.two_player_p1_minus_poison);
+        TextView player1PoisonView = (TextView) findViewById(R.id.two_player_p1_poison);
+        Button player1PlusPoison = (Button) findViewById(R.id.two_player_p1_plus_poison);
+        Button player2MinusOneHealth = (Button) findViewById(R.id.two_player_p2_minus_onehealth);
+        Button player2MinusFiveHealth = (Button) findViewById(R.id.two_player_p2_minus_fivehealth);
+        TextView player2HealthView = (TextView) findViewById(R.id.two_player_p2_health);
+        Button player2PlusOneHealth = (Button) findViewById(R.id.two_player_p2_plus_onehealth);
+        Button player2PlusFiveHealth = (Button) findViewById(R.id.two_player_p2_plus_fivehealth);
+        Button player2MinusEnergy = (Button) findViewById(R.id.two_player_p2_minus_energy);
+        TextView player2EnergyView = (TextView) findViewById(R.id.two_player_p2_energy);
+        Button player2PlusEnergy = (Button) findViewById(R.id.two_player_p2_plus_energy);
+        Button player2MinusPoison = (Button) findViewById(R.id.two_player_p2_minus_poison);
+        TextView player2PoisonView = (TextView) findViewById(R.id.two_player_p2_poison);
+        Button player2PlusPoison = (Button) findViewById(R.id.two_player_p2_plus_poison);
+
+        // OnClickListeners for buttons
+        player1MinusOneHealth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                healthPlayer1 = healthPlayer1 - 1;
+                refreshPlayer1Health();
+            }
+        });
+        player1MinusFiveHealth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                healthPlayer1 = healthPlayer1 - 5;
+                refreshPlayer1Health();
+            }
+        });
+
+        player1PlusOneHealth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                healthPlayer1 = healthPlayer1 + 1;
+                refreshPlayer1Health();
+            }
+        });
+
+        player1PlusFiveHealth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                healthPlayer1 = healthPlayer1 + 5;
+                refreshPlayer1Health();
+            }
+        });
+
+        player1MinusEnergy.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                energyPlayer1 = energyPlayer1 - 1;
+                refreshPlayer1Energy();
+            }
+        });
+
+        player1PlusEnergy.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                energyPlayer1 = energyPlayer1 + 1;
+                refreshPlayer1Energy();
+            }
+        });
+
+        player1MinusPoison.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                poisonPlayer1 = poisonPlayer1 - 1;
+                refreshPlayer1Poison();
+            }
+        });
+
+        player1PlusPoison.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                poisonPlayer1 = poisonPlayer1 + 1;
+                refreshPlayer1Poison();
+            }
+        });
+
+        player2MinusOneHealth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                healthPlayer2 = healthPlayer2 - 1;
+                refreshPlayer2Health();
+            }
+        });
+        player2MinusFiveHealth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                healthPlayer2 = healthPlayer2 - 5;
+                refreshPlayer2Health();
+            }
+        });
+
+        player2PlusOneHealth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                healthPlayer2 = healthPlayer2 + 1;
+                refreshPlayer2Health();
+            }
+        });
+
+        player2PlusFiveHealth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                healthPlayer2 = healthPlayer2 + 5;
+                refreshPlayer2Health();
+            }
+        });
+
+        player2MinusEnergy.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                energyPlayer2 = energyPlayer2 - 1;
+                refreshPlayer2Energy();
+            }
+        });
+
+        player2PlusEnergy.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                energyPlayer2 = energyPlayer2 + 1;
+                refreshPlayer2Energy();
+            }
+        });
+
+        player2MinusPoison.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                poisonPlayer2 = poisonPlayer2 - 1;
+                refreshPlayer2Poison();
+            }
+        });
+
+        player2PlusPoison.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                poisonPlayer2 = poisonPlayer2 + 1;
+                refreshPlayer2Poison();
+            }
+        });
     }
+
+    private void refreshPlayer1Health() {
+        TextView player1HealthView = (TextView) findViewById(R.id.two_player_p1_health);
+        player1HealthView.setText(String.valueOf(healthPlayer1));
+    }
+    private void refreshPlayer1Energy() {
+        TextView player1EnergyView = (TextView) findViewById(R.id.two_player_p1_energy);
+        player1EnergyView.setText(String.valueOf(energyPlayer1));
+    }
+    private void refreshPlayer1Poison() {
+        TextView player1PoisonView = (TextView) findViewById(R.id.two_player_p1_poison);
+        player1PoisonView.setText(String.valueOf(poisonPlayer1));
+    }
+    private void refreshPlayer2Health() {
+        TextView player2HealthView = (TextView) findViewById(R.id.two_player_p2_health);
+        player2HealthView.setText(String.valueOf(healthPlayer2));
+    }
+    private void refreshPlayer2Energy() {
+        TextView player2EnergyView = (TextView) findViewById(R.id.two_player_p2_energy);
+        player2EnergyView.setText(String.valueOf(energyPlayer2));
+    }
+    private void refreshPlayer2Poison() {
+        TextView player2PoisonView = (TextView) findViewById(R.id.two_player_p2_poison);
+        player2PoisonView.setText(String.valueOf(poisonPlayer2));
+    }
+
 }
