d987947c5bbf50103d0f977b64904ec8e92a9220
stevenconner <steven.c.conner@gmail.com>
2016-12-07 16:26:20 -0800

    initial commit, basic UI done

---
+++ app/src/main/java/sigildesigns/materiallifecounter/TwoPlayerActivity.java
@@ -0,0 +1,13 @@
+package sigildesigns.materiallifecounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TwoPlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two_player);
+    }
+}
